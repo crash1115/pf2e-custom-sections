f6d455d0b1a1c18b8854a2e01f029030d07f026e
Chelsea Ash <crash1115@yahoo.com>
2024-11-30 13:58:26 -0500

    Adjust header styling

--- scripts/pf2e-custom-sections.js
+++ scripts/pf2e-custom-sections.js
@@ -1,7 +1,10 @@
 import { libWrapper } from "./libwrapper-shim.js";
-
 const MODULE_NAME = "pf2e-custom-sections";
 
+
+/////////////////////////////////
+// ACTOR SHEET DATA MODIFICATIONS
+/////////////////////////////////
 
 Hooks.once("ready", () => {
    libWrapper.register(
@@ -11,15 +14,6 @@ Hooks.once("ready", () => {
         "WRAPPER"
     );
 });
-
-Hooks.on(`renderFeatSheetPF2e`, (app, html, data) => {    
-    addCustomSectionField(app, html, data);
-});
-
-Hooks.on(`renderAbilitySheetPF2e`, (app, html, data) => {    
-    addCustomSectionField(app, html, data);
-});
-
 
 async function addCustomSectionsToData(wrapped, options){
     const data = (await wrapped(options));
@@ -73,7 +67,7 @@ function createSectionData (actions, actor){
         // If the section doesn't already exist, create it
         let sectionIndex = sections.findIndex(s => s.label === sectionName);          
         if(sectionIndex === -1){
-            sections.push({ label: sectionName, actions:[] });
+            sections.push({ label: sectionName, actions:[], isPf2eCustomSection:true });
             sectionIndex = sections.length - 1;
         }
 
@@ -84,8 +78,59 @@ function createSectionData (actions, actor){
 }
 
 /////////////////////////////////
+// ACTOR SHEET MODIFICATIONS
+/////////////////////////////////
+
+Hooks.on(`renderActorSheetPF2e`, (app, html, data) => {    
+    addClassesToHeaders(app, html, data);
+});
+
+function addClassesToHeaders (app, html, data) {
+    // Get list of custom section labels
+    const encounterSections = data.actions.encounter;
+    let customSectionLabels = [];
+    for (const property in encounterSections){
+        const section = encounterSections[property];
+        if(section.isPf2eCustomSection){
+            customSectionLabels.push(section.label)
+        }
+    }
+
+    // Get headers in encounter panel
+    const actionPanels = html.find('.actions-panel');
+    const encounterPanel = actionPanels.filter('[data-tab="encounter"]');
+    const headers = encounterPanel.find('header');
+
+    // Manipulate the custom section headers
+    for(var i=0; i<headers.length; i++){
+        const header = headers[i];
+        const label = header.innerText.trim(" ").split("\n")[0];
+
+        if(customSectionLabels.includes(label)){
+            // Assign class to header element
+            $(header).addClass('pf2e-custom-section');
+
+            // Remove controls
+            $(header).children('.controls').remove();
+
+            // Inject indicator tag in place of buttons
+            const customTag = $("<div class='custom-section-tag'>Custom Section</div>");
+            $(header).append(customTag);
+        }
+    } 
+}
+
+/////////////////////////////////
 // ITEM SHEET MODIFICATIONS
 /////////////////////////////////
+
+Hooks.on(`renderFeatSheetPF2e`, (app, html, data) => {    
+    addCustomSectionField(app, html, data);
+});
+
+Hooks.on(`renderAbilitySheetPF2e`, (app, html, data) => {    
+    addCustomSectionField(app, html, data);
+});
 
 async function addCustomSectionField(app, html, data) {
     // Don't activate this if the item is a downtime or exploration activity or an item
@@ -113,8 +158,9 @@ function activateTabListeners(html, data){
     }); 
 }
 
+
 /////////////////////////////////
-// UTILITY
+// UTILITY METHODS
 /////////////////////////////////
 
 function slugify(str) {
