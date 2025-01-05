511d6b7c2d7ee2c161594af0771bf7d4437be119
Chelsea Ash <crash1115@yahoo.com>
2025-01-05 14:41:41 -0500

    Just ADHD Things

--- scripts/pf2e-custom-sections.js
+++ scripts/pf2e-custom-sections.js
@@ -1,7 +1,6 @@
 import { libWrapper } from "./libwrapper-shim.js";
 const MODULE_NAME = "pf2e-custom-sections";
 
-
 /////////////////////////////////
 // PC SHEET DATA MODIFICATIONS
 /////////////////////////////////
@@ -132,7 +131,6 @@ function addClassesToHeaders (app, html, data) {
     } 
 }
 
-
 /////////////////////////////////
 // ITEM SHEET MODIFICATIONS
 /////////////////////////////////
@@ -157,7 +155,7 @@ async function addCustomSectionField(app, html, data) {
 
     // Inject content into to the sheet sidebar
     let sidebar = html.find('.inventory-details');
-    let sectionInput = $(await renderTemplate('modules/pf2e-custom-sections/templates/feat-sheet-input.hbs', data.item.flags[MODULE_NAME]))
+    let sectionInput = $(await renderTemplate('modules/pf2e-custom-sections/templates/feat-sheet-input.hbs', data.item.flags[MODULE_NAME]));
     sidebar.append(sectionInput);
     activateTabListeners(html, data);   
 }
